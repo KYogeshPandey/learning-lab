c34e236319abeb09ac22f6f56af93305a91e67fb
Yogesh Pandey <yogeshhpandey2004@gmail.com>
2026-03-12 19:52:51 +0530

    learned new

---
+++ shutil_module.py
@@ -0,0 +1,10 @@
+import shutil
+import os
+
+# shutil.copy("shutil_module.py", "shutil_module_copy.py")
+
+# shutil.copytree("clutteredFolder", "clutteredFolderCopy")
+
+# shutil.move("1.png","clutteredFolder/1.png")
+
+# shutil.rmtree("clutteredFolderCopy") 
---
+++ walrus_operator.py
@@ -0,0 +1,26 @@
+# a= True
+print(a:=False)
+
+n = [1,2,3,4,5]
+
+while (i := (len(n))):
+    print(n.pop())
+
+
+
+# foods = list()
+# while True:
+#     food = input("What food do you like ?:")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+# print(f"the list of foods you like is:{foods}")
+
+#using walrus operator
+
+foods = list()
+while(food:= input("what food do you like ?:")) != "quit":
+    foods.append(food)
+
+print(f"the list of foods you like is:{foods}")
