3db5873eeabf9513565fb3278b3eb0961802d8ad
Yogesh Pandey <yogeshhpandey2004@gmail.com>
2026-03-17 18:59:41 +0530

    91

--- request_module.py
+++ request_module.py
@@ -1,5 +1,24 @@
 import requests
 
+# GET request
 response = requests.get("https://www.youtube.com/")
 print(response.status_code)
-print(response.text)    
+print(response.text)
+
+# POST request
+url = "https://jsonplaceholder.typicode.com/posts"
+
+data = {
+    "title": "foo",
+    "body": "bar",
+    "userId": 1
+}
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json=data, headers=headers)
+
+print(response.status_code)
+print(response.json())
