2612862b0f32c971b17a8ed8559e009f977de7e8
Yogesh Pandey <yogeshhpandey2004@gmail.com>
2026-03-22 22:41:30 +0530

    practice

---
+++ practice/Day1.py
@@ -0,0 +1,40 @@
+#take input from users and store them in a variable
+# add the two variables
+#print result
+
+
+# num1 = int(input('enter first number :'))
+# num2 = int(input('enter second number :'))
+# result = num1 + num2
+# print(result)
+# print(type(num1))
+
+# num1 = input('number :')
+# num2 = input('number :')
+# result = int(num1) + int(num2)
+# print(result)
+# print(type(num1))
+
+a = 0b1010    #binary literal
+b = 100       #decimal literal
+c = 0o310     #octal literal
+d = 0x12c     #hexadecimal literal
+
+#float literal
+float_1 = 10.5
+float_2 = 2.5e2
+float_3 = 1.5e-2
+
+#complex literals
+x = 3.14j   
+
+unicode = u"U0001f600"
+print(x.real, x.imag)
+
+e = True + 4
+f = False +10
+print(e)
+print(f)
+g= None
+print(g)
+print(a) 
