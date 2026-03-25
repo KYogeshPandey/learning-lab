ca9b868141c91e9b9c2e2e89e4ff47d8149f603c
Yogesh Pandey <yogeshhpandey2004@gmail.com>
2026-03-25 19:07:49 +0530

    practice

---
+++ practice/Day_03/p3.py
@@ -0,0 +1,17 @@
+#module in  Python
+
+import math
+import keyword
+import random
+import datetime
+
+
+math.factorial(5)
+math.floor(5.7)
+
+print(keyword.kwlist)
+print(random.randint(1,1000))
+print(datetime.datetime.now())
+
+help('modules')
+
