ef0894b34e83d10b4485883cfc7c5c81fc0b7aa0
Yogesh Pandey <yogeshhpandey2004@gmail.com>
2026-07-09 14:39:51 +0530

    changed the path of the folder

--- practice/Libraries/Day_27(Pandas)/p2.py
+++ practice/Libraries/Day_27(Pandas)/p2.py
@@ -3,14 +3,14 @@ import pandas as pd
 import numpy as np
 
 # With one column
-subs = pd.read_csv(r'C:\Users\HP\Desktop\python progamming\practice\Day_27(Pandas)\subs.csv').squeeze('columns')
+subs = pd.read_csv(r'C:\Users\HP\Desktop\python progamming\practice\Libraries\Day_27(Pandas)\subs.csv').squeeze('columns')
 print(subs)
 
 # With two column
-kohli = pd.read_csv(r'C:\Users\HP\Desktop\python progamming\practice\Day_27(Pandas)\kohli_ipl.csv',index_col = 'match_no').squeeze('columns')
+kohli = pd.read_csv(r'C:\Users\HP\Desktop\python progamming\practice\Libraries\Day_27(Pandas)\kohli_ipl.csv',index_col = 'match_no').squeeze('columns')
 print(kohli)
 
-movies = pd.read_csv(r'C:\Users\HP\Desktop\python progamming\practice\Day_27(Pandas)\bollywood.csv',index_col = 'movie').squeeze('columns')
+movies = pd.read_csv(r'C:\Users\HP\Desktop\python progamming\practice\Libraries\Day_27(Pandas)\bollywood.csv',index_col = 'movie').squeeze('columns')
 print(movies)
 
 
